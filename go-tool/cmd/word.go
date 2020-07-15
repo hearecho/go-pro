7c727db605f4a5f5a9a51b1dbbb21fb7b7d3a904
echo <1540302560@qq.com>
2020-07-15 21:34:04 +0800

    go-tool使用cobra制作的cli工具

---
+++ go-tool/cmd/word.go
@@ -0,0 +1,57 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"go-tool/utils"
+	"log"
+	"strings"
+)
+
+const (
+	ModeUpper                      = iota + 1 // 全部转大写
+	ModeLower                                 // 全部转小写
+	ModeUnderscoreToUpperCamelCase            // 下划线转大写驼峰
+	ModeUnderscoreToLowerCamelCase            // 下线线转小写驼峰
+	ModeCamelCaseToUnderscore                 // 驼峰转下划线
+)
+
+var str string
+var mode int8
+var desc = strings.Join([]string{
+	"该子命令支持各种单词格式转换，模式如下：",
+	"1：全部转大写",
+	"2：全部转小写",
+	"3：下划线转大写驼峰",
+	"4：下划线转小写驼峰",
+	"5：驼峰转下划线",
+}, "\n")
+
+var wordCmd = &cobra.Command{
+	Use:   "word",
+	Short: "单词格式转换",
+	Long:  desc,
+	Run: func(cmd *cobra.Command, args []string) {
+		var content string
+		switch mode {
+		case ModeUpper:
+			content = utils.ToUpper(str)
+		case ModeLower:
+			content = utils.ToLower(str)
+		case ModeUnderscoreToUpperCamelCase:
+			content = utils.UnderscoreToUpperCamelCase(str)
+		case ModeUnderscoreToLowerCamelCase:
+			content = utils.UnderscoreToLowerCamelCase(str)
+		case ModeCamelCaseToUnderscore:
+			content = utils.CamelCaseToUnderscore(str)
+		default:
+			log.Fatalf("暂不支持该转换模式，请执行 help word 查看帮助文档")
+		}
+
+		log.Printf("输出结果: %s", content)
+	},
+}
+
+func init() {
+	wordCmd.Flags().StringVarP(&str, "str", "s", "", "请输入单词内容")
+	wordCmd.Flags().Int8VarP(&mode, "mode", "m", 0, "请输入单词转换的模式")
+}
