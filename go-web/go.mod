2db6225ef1f8b1f457f01334aa8c1d4bfc7f15cd
echo <1540302560@qq.com>
2020-07-24 12:03:03 +0800

    jwt鉴权

--- go-web/go.mod
+++ go-web/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/astaxie/beego v1.12.2
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.15
--- go-web/routers/router.go
+++ go-web/routers/router.go
@@ -2,7 +2,9 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/hearecho/go-pro/go-web/middleware/jwt"
 	"github.com/hearecho/go-pro/go-web/pkg/setting"
+	"github.com/hearecho/go-pro/go-web/routers/api"
 	v1 "github.com/hearecho/go-pro/go-web/routers/api/v1"
 )
 
@@ -12,7 +14,9 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth",api.GetAuth)
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		apiv1.GET("/tags",v1.GetTags)
 		apiv1.POST("/tags",v1.AddTag)
